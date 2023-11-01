e6b247b7bdb460fc238cc3de591e6620ad8bafdf
kxk16 <141895330+kxk16@users.noreply.github.com>
2023-11-01 16:14:33 +0530

    New app added (with Login cred pad=ge)
    TML_Demo_App_3 added with the login page creds (proto)

---
+++ TML_Demo_App_3/app/src/main/java/com/example/tml_demo_app_3/ThirdActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tml_demo_app_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
